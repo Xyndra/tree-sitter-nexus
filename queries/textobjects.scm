564278cc0d1da0199b50982969709685ee4f89a1
Xyndra <71836523+Xyndra@users.noreply.github.com>
2025-11-30 01:00:12 +0100

    Update Queries

--- queries/textobjects.scm
+++ queries/textobjects.scm
@@ -1,90 +1,56 @@
 ; Text objects for Vim-style navigation
+; Zed only supports: @function.around, @function.inside, @class.around, @class.inside, @comment.around, @comment.inside
 
 ; Functions
 (function_definition
-  body: (block
-    "{" @_start
+  (block
+    "{"
     (_)* @function.inside
-    "}" @_end)) @function.around
+    "}")) @function.around
 
 (function_definition) @function.around
 
 ; Methods
 (method_definition
-  body: (block
-    "{" @_start
+  (block
+    "{"
     (_)* @function.inside
-    "}" @_end)) @function.around
+    "}")) @function.around
 
 (method_definition) @function.around
 
-; Macro definitions
+; Macro definitions (treated as functions)
 (macro_definition
-  body: (block
-    "{" @_start
+  (block
+    "{"
     (_)* @function.inside
-    "}" @_end)) @function.around
+    "}")) @function.around
 
 (macro_definition) @function.around
 
+; Subscopes (also function-like for navigation)
+(subscope_statement
+  (block
+    "{"
+    (_)* @function.inside
+    "}")) @function.around
+
 ; Structs (as class-like constructs)
 (struct_definition
-  "{" @_start
+  "{"
   (_)* @class.inside
-  "}" @_end) @class.around
+  "}") @class.around
 
 (struct_definition) @class.around
 
 ; Interfaces (as class-like constructs)
 (interface_definition
-  "{" @_start
+  "{"
   (_)* @class.inside
-  "}" @_end) @class.around
+  "}") @class.around
 
 (interface_definition) @class.around
 
 ; Comments
 (comment) @comment.around
 (comment)+ @comment.around
-
-; Parameters
-(parameter) @parameter.inside
-(parameter_list
-  "(" @_start
-  (parameter)? @parameter.inside
-  ")" @_end) @parameter.around
-
-; Arguments
-(argument_list
-  "(" @_start
-  (_)? @parameter.inside
-  ")" @_end) @parameter.around
-
-; Blocks
-(block
-  "{" @_start
-  (_)* @block.inside
-  "}" @_end) @block.around
-
-; Subscopes (also function-like for navigation)
-(subscope_statement
-  body: (block
-    "{" @_start
-    (_)* @function.inside
-    "}" @_end)) @function.around
-
-; Conditionals
-(if_statement) @conditional.around
-(if_statement
-  (block
-    "{" @_start
-    (_)* @conditional.inside
-    "}" @_end))
-
-; Loops (subscopes are the loop construct in Nexus)
-(subscope_statement) @loop.around
-(subscope_statement
-  body: (block
-    "{" @_start
-    (_)* @loop.inside
-    "}" @_end))
